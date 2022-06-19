ceaaa4ffb92a21f18ccd5939a977f6d685a2e894
A.C.E <bkonuoha8@gmail.com>
2022-06-19 22:31:07 +0100

    meet task requirements

---
+++ I4G006427LYU/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
+
+# Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField(max_length=1000)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_Date = models.DateTimeField()
+    Published_Date = models.DateTimeField()
+    
